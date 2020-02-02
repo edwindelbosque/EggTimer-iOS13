c0d13d478f15ceb89703c786af860eb054784ce5
Edwin Del Bosque <edwindelbosquer@gmail.com>
2020-02-02 02:36:26 -0700

    Add switch case and data to project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,26 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes : [String: Int] = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12
+    ]
+    
+    @IBAction func EggButtonPressed(_ sender: UIButton) {
+//        switch sender.currentTitle {
+//        case "Soft":
+//            print(5)
+//        case "Medium":
+//            print(7)
+//        case "Hard":
+//            print(12)
+//        default:
+//            print("Error: Could not find hardness")
+//        }
+        print(eggTimes[sender.currentTitle!]!)
+    }
+    
 
 }
